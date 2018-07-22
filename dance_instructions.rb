abcbdcc9db8818041f84ca984ab0d3be3b6035de
Cece Zinny <cwzinny@gmail.com>
2018-07-22 22:19:50 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -18,6 +18,6 @@
   def bob_the_reins
     puts "Holding your arms out in front of you, straight and at chest level"
     puts "Cross your right wrist over your left and hold them together"
-    puts ""
+    puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
   end 
    
