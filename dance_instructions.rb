e936e224e9bef6a82265f9cccb2c0c1e621c740e
Cece Zinny <cwzinny@gmail.com>
2018-07-22 22:12:23 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -5,7 +5,9 @@
   
   def base_footwork
     puts "Lift right foot"
-    puts ""
+    puts "Return right foot to the ground"
+    puts "Finish with a small skip-step backward"
+  end 
   
   def skip_step
     puts "Skip a step"
