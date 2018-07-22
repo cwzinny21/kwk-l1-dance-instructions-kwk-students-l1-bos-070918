0afd95c377f5787df2d749ab359ec6f886d5157e
Cece Zinny <cwzinny@gmail.com>
2018-07-22 22:17:53 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -12,7 +12,7 @@
   def skip_step(starting_foot)
     puts "Lower #{starting_foot} to the ground"
     puts "Bounce #{starting_foot} back up slightly, kicking it a few inches back"
-    puts "Left, Right, Left and Left"
+    # puts "Left, Right, Left and Left"
     
   end 
    
